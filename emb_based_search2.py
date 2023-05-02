7a89ce0a9f3651fb82a09d10143d9adce809f520
rj24115 <roman.jarocki@gmail.com>
2023-05-02 23:23:31 +0100

    initial local commit

---
+++ emb_based_search2.py
@@ -0,0 +1,159 @@
+# MVP for OpenAI GPT search on internal dataset of embeddings 
+
+# imports
+import ast  # for converting embeddings saved as strings back to arrays
+import openai  # for calling the OpenAI API
+import pandas as pd  # for storing text and embeddings data
+import tiktoken  # for counting tokens
+from scipy import spatial  # for calculating vector similarities for search
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import json
+
+# models
+EMBEDDING_MODEL = "text-embedding-ada-002"
+GPT_MODEL = "gpt-3.5-turbo"
+
+# keys
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# import dataset with embeddings
+embeddings_path = "hc3_embedded.csv"
+df = pd.read_csv(embeddings_path)
+
+# convert embeddings from CSV str type back to list type
+df['embedding'] = df['embedding'].apply(ast.literal_eval)
+
+
+# search function
+def strings_ranked_by_relatedness(
+    query: str,
+    df: pd.DataFrame,
+    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+    top_n: int = 100
+) -> tuple[list[str], list[float]]:
+    """Returns a list of strings and relatednesses, sorted from most related to least."""
+    query_embedding_response = openai.Embedding.create(
+        model=EMBEDDING_MODEL,
+        input=query,
+    )
+    query_embedding = query_embedding_response["data"][0]["embedding"]
+    strings_and_relatednesses = [
+        (row["topic_answer"], relatedness_fn(query_embedding, row["embedding"]))
+        for i, row in df.iterrows()
+    ]
+    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+    strings, relatednesses = zip(*strings_and_relatednesses)
+    return strings[:top_n], relatednesses[:top_n]
+
+
+def num_tokens(text: str, model: str = GPT_MODEL) -> int:
+    """Return the number of tokens in a string."""
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(text))
+
+
+def query_message(
+    query: str,
+    df: pd.DataFrame,
+    model: str,
+    token_budget: int
+) -> str:
+    """Return a message for GPT, with relevant source texts pulled from a dataframe."""
+    strings, relatednesses = strings_ranked_by_relatedness(query, df)
+    introduction = 'Use the below articles from the Creditspring help center articles to answer the subsequent question. If the answer cannot be found in the articles, write "Please contact our Help Centre for more information."'
+    question = f"\n\nQuestion: {query}"
+    message = introduction
+    for string in strings:
+        next_article = f'\n\nArticle section:\n"""\n{string}\n"""'
+        if (
+            num_tokens(message + next_article + question, model=model)
+            > token_budget
+        ):
+            break
+        else:
+            message += next_article
+    return message + question
+
+ 
+# Act as a Creditspring UK chatbot answering members' questions about loan product memberships offered by Creditspring.  
+# "You answer questions about loan product memberships offered by Creditspring"
+
+def ask(
+    query: str,
+    df: pd.DataFrame = df,
+    model: str = GPT_MODEL,
+    token_budget: int = 4096 - 500,
+    print_message: bool = False,
+) -> str:
+    """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
+    message = query_message(query, df, model=model, token_budget=token_budget)
+    if print_message:
+        print(message)
+    messages = [
+        {"role": "system", "content": "You are a helpful Creditspring help desk employee. "},
+        {"role": "user", "content": message},
+    ]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.0
+    )
+    response_message = response["choices"][0]["message"]["content"]
+    # return response_message
+    return response
+
+
+# streamlit renders
+
+response = False
+prompt_tokens = 0
+completion_tokes = 0
+total_tokens_used = 0
+cost_of_response = 0
+
+
+st.markdown("[![Foo](https://www.creditspring.co.uk/assets/logo-05a1ba38ca9388fcd14f8e2a3c9c6efb2830f7ce677cb9990a631f814c5b2ef7.svg)](https://www.creditspring.co.uk)")
+st.header("Creditspring Help Centre + OpenAI ChatGPT API")
+st.markdown("""---""")
+
+question_input = st.text_input("How can we help you?")
+rerun_button = st.button("Rerun")
+
+st.markdown("""---""")
+
+
+if question_input:
+    response = ask(question_input)
+else:
+    pass
+
+if rerun_button:
+    response = ask(question_input)
+else:
+    pass
+
+if response:
+    st.write("Response:")
+    st.write(response["choices"][0]["message"]["content"])
+    # st.write( response)
+    
+    prompt_tokens = response["usage"]["prompt_tokens"]
+    completion_tokes = response["usage"]["completion_tokens"]
+    total_tokens_used = response["usage"]["total_tokens"]
+
+    cost_of_response = total_tokens_used * 0.000002
+else:
+    pass
+
+
+with st.sidebar:
+    st.title("Usage Stats:")
+    st.markdown("""---""")
+    st.write("Promt tokens used :", prompt_tokens)
+    st.write("Completion tokens used :", completion_tokes)
+    st.write("Total tokens used :", total_tokens_used)
+    st.write("Total cost of request: ${:.8f}".format(cost_of_response))
+
